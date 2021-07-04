378aa302ea06cd066622ed981d2a06c7a8f7b476
NirjaRoy <56375771+NirjaRoy@users.noreply.github.com>
2021-07-04 16:58:47 +0530

    CRUD functionality for Property API

---
+++ src/main/java/com/app/staycation/StaycationApplication.java
@@ -0,0 +1,13 @@
+package com.app.staycation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaycationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaycationApplication.class, args);
+	}
+
+}
